31a0da9674a44f75506574a0b6296424a1e38558
Vlad-Stefan Harbuz <vlad@vladh.net>
2022-01-26 20:51:53 +0100

    add libserialport demo

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <libserialport.h>
+
 
 int main() {
-    printf("Hello world!\n");
-    getchar();
+    struct sp_port **port_list;
+    if (sp_list_ports(&port_list) != SP_OK) {
+        printf("sp_list_ports() failed!\n");
+        return NULL;
+    }
+
+    for (unsigned idx = 0; port_list[idx] != NULL; idx++) {
+        struct sp_port *port = port_list[idx];
+        char *port_name = sp_get_port_name(port);
+        char *port_manuf = sp_get_port_usb_manufacturer(port);
+        printf("%s (manufacturer: %s)\n", port_name, port_manuf);
+    }
+
+    sp_free_port_list(port_list);
 }
--- make_windows.mk
+++ make_windows.mk
@@ -1,12 +1,14 @@
 COMPILER_FLAGS = \
+	-I "C:/local/include/"  \
 	-FC -GL -GS -MP4 -O2 -Oi -W4 -WX -WL -Z7 -Zo -diagnostics:column \
 	-fp:fast -nologo -sdl -guard:cf -permissive- -std:c11 \
-	-wd4996
+	-wd4996 -wd4189
 
 LINKER_FLAGS = \
+	-LIBPATH:"C:/local/lib/" \
 	/SUBSYSTEM:console \
 	-CGTHREADS:8 -DEBUG:FULL -INCREMENTAL:no -opt:ref \
-	user32.lib gdi32.lib shell32.lib
+	libserialport.lib user32.lib gdi32.lib shell32.lib
 
 .PHONY: main run clean
 
