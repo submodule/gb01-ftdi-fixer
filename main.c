8c0e14e43d0d77877b1e86a1d902136d23627436
Vlad-Stefan Harbuz <vlad@vladh.net>
2022-01-27 11:28:07 +0100

    add ft_prog code

--- main.c
+++ main.c
@@ -6,54 +6,120 @@
 #include <libserialport.h>
 
 
-#define FT_PATH "tools\\windows\\ft232r_prog\\ft232r_prog.exe"
+#define FT_PATH "tools\\windows\\ft_prog\\FT_Prog-CmdLine.exe"
+#define FT_XML_PATH "gb01.xml"
 #define AVRDUDE_PATH "tools\\windows\\avrdude\\bin\\avrdude"
 #define AVRDUDE_CONF_PATH "tools/windows/avrdude/etc/avrdude.conf"
 #define FIRMWARE_PATH "data/gb01-firmware.hex"
-#define SERIAL_LENGTH 9 + 3
-
-
-void print_serial_ports(struct sp_port **port_list) {
-    for (int idx = 0; port_list[idx] != NULL; idx++) {
-        struct sp_port *port = port_list[idx];
-        char *port_name = sp_get_port_name(port);
-        char *port_manuf = sp_get_port_usb_manufacturer(port);
-        printf("  * [%d] %s (manufacturer: %s)\n", idx + 1, port_name, port_manuf);
-    }
-}
+#define SERIAL_LENGTH 9
+#define XML_PART_1 \
+    "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" \
+    "<FT_EEPROM>\n" \
+      "<Chip_Details>\n" \
+        "<Type>FT232R</Type>\n" \
+      "</Chip_Details>\n" \
+      "<USB_Device_Descriptor>\n" \
+        "<VID_PID>0</VID_PID>\n" \
+        "<idVendor>0403</idVendor>\n" \
+        "<idProduct>6001</idProduct>\n" \
+        "<bcdUSB>USB 2.0</bcdUSB>\n" \
+      "</USB_Device_Descriptor>\n" \
+      "<USB_Config_Descriptor>\n" \
+        "<bmAttributes>\n" \
+          "<RemoteWakeupEnabled>true</RemoteWakeupEnabled>\n" \
+          "<SelfPowered>false</SelfPowered>\n" \
+          "<BusPowered>true</BusPowered>\n" \
+        "</bmAttributes>\n" \
+        "<IOpullDown>false</IOpullDown>\n" \
+        "<MaxPower>90</MaxPower>\n" \
+      "</USB_Config_Descriptor>\n" \
+      "<USB_String_Descriptors>\n" \
+        "<Manufacturer>Submodule</Manufacturer>\n" \
+        "<Product_Description>GB01</Product_Description>\n" \
+        "<SerialNumber_Enabled>true</SerialNumber_Enabled>\n" \
+        "<SerialNumber>SUB"
+#define XML_PART_2 \
+            "</SerialNumber>\n" \
+        "<SerialNumberPrefix>SU</SerialNumberPrefix>\n" \
+        "<SerialNumber_AutoGenerate>false</SerialNumber_AutoGenerate>\n" \
+      "</USB_String_Descriptors>\n" \
+      "<Hardware_Specific>\n" \
+        "<HighIO>false</HighIO>\n" \
+        "<D2XX>false</D2XX>\n" \
+        "<ExternalOscillator>false</ExternalOscillator>\n" \
+        "<Invert_RS232_Signals>\n" \
+          "<TXD>false</TXD>\n" \
+          "<RXD>false</RXD>\n" \
+          "<RTS>false</RTS>\n" \
+          "<CTS>false</CTS>\n" \
+          "<DTR>false</DTR>\n" \
+          "<DSR>false</DSR>\n" \
+          "<DCD>false</DCD>\n" \
+          "<RI>false</RI>\n" \
+        "</Invert_RS232_Signals>\n" \
+        "<IO_Controls>\n" \
+          "<C0>TXLED#</C0>\n" \
+          "<C1>RXLED#</C1>\n" \
+          "<C2>TXDEN</C2>\n" \
+          "<C3>PWREN#</C3>\n" \
+          "<C4>SLEEP#</C4>\n" \
+        "</IO_Controls>\n" \
+      "</Hardware_Specific>\n" \
+    "</FT_EEPROM>\n"
 
 
 void make_serial(char *buf) {
-    buf[0] = 'S';
-    buf[1] = 'U';
-    buf[2] = 'B';
-    for (int idx = 3; idx < SERIAL_LENGTH; idx++) {
+    for (int idx = 0; idx < SERIAL_LENGTH; idx++) {
         buf[idx] = (rand() % 10) + '0';
     }
     buf[SERIAL_LENGTH] = '\0';
 }
 
 
-void make_ft_command(char *ft_command, char *serial) {
-    strcpy(ft_command, FT_PATH " --manufacturer Test --product Test --new-serial-number test1234");
-    /* strcpy(ft_command, FT_PATH " --manufacturer Submodule --product GB01 --new-serial-number "); */
-    /* strcat(ft_command, serial); */
+void make_xml(char *buf) {
+    char serial[SERIAL_LENGTH + 1];
+    make_serial(serial);
+
+    strcpy(buf, XML_PART_1);
+    strcat(buf, serial);
+    strcat(buf, XML_PART_2);
 }
 
 
-void make_avrdude_command(char *avrdude_command, char *port_name) {
-    strcpy(avrdude_command, AVRDUDE_PATH " -q -V -D -C " AVRDUDE_CONF_PATH " -c arduino -p atmega32 -P ");
-    strcat(avrdude_command, port_name);
-    strcat(avrdude_command, " -b 115200 -U flash:w:" FIRMWARE_PATH ":i");
+void make_ft_scan_command(char *command) {
+    strcpy(command, FT_PATH " scan");
+}
+
+
+void make_ft_write_command(char *command, int idx) {
+    char idx_as_str[32];
+    sprintf(idx_as_str, "%d", idx);
+
+    strcpy(command, FT_PATH " scan prog ");
+    strcat(command, idx_as_str);
+    strcat(command, " " FT_XML_PATH);
+}
+
+
+void make_avrdude_command(char *command, char *port_name) {
+    strcpy(command, AVRDUDE_PATH " -q -V -D -C " AVRDUDE_CONF_PATH " -c arduino -p atmega32 -P ");
+    strcat(command, port_name);
+    strcat(command, " -b 115200 -U flash:w:" FIRMWARE_PATH ":i");
 }
 
 
 int main() {
     srand((unsigned)time(NULL));
 
-    char serial[SERIAL_LENGTH + 1];
-    make_serial(serial);
-    printf("%s\n", serial);
+    char xml[4096];
+    make_xml(xml);
+    FILE *f = fopen(FT_XML_PATH, "w");
+    if (f == NULL) {
+        printf("Could not open " FT_XML_PATH ", make sure you have sufficient permissions.");
+        goto cleanup;
+    }
+    fputs(xml, f);
+    fclose(f);
 
     printf(
         "#\n"
@@ -63,56 +129,50 @@ int main() {
         "This tool allows you to update your Submodule's GB01's FTDI information (and firmware).\n"
         "First of all, let's find your GB01.\n"
         "\n"
-        "Currently connected serial devices: \n"
-        "\n"
     );
 
-    struct sp_port **port_list;
-    if (sp_list_ports(&port_list) != SP_OK) {
-        printf("Could not list serial ports! Please contact us at support@submodule.co.\n");
+    char command[1024];
+    int command_status;
+    make_ft_scan_command(command);
+    command_status = system(command);
+
+    if (command_status != 0) {
+        printf("There was an issue with searching for devices.\n");
+        printf("Please contact us at support@submodule.co.\n");
         goto cleanup;
     }
 
-    int n_ports = 0;
-    do {
-        if (port_list[n_ports] == NULL) {
-            break;
-        }
-        n_ports++;
-    } while (true);
-
-    print_serial_ports(port_list);
-
     printf(
         "\n"
-        "Please type the number in front of your GB01 (1, 2, 3, ...) then press enter.\n"
+        "Please type the number in front of your GB01 (0, 1, 2, ...) then press enter.\n"
+        "The GB01 will say \"FT232R\". If there's only one device, it's probably the GB01.\n"
         "If you don't see your GB01, make sure it's connected, close this window (or type \"exit\"), and restart the program.\n"
         "If you still can't find it, close this window (or type \"exit\") and contact us at support@submodule.co.\n"
         "\n"
     );
 
-    printf("GB01 location > ");
-    int selected_port = 0;
-    scanf("%u", &selected_port);
+    printf("GB01 device > ");
+    int selected_port = -1;
+    scanf("%d", &selected_port);
+    printf("\n");
 
-    if (selected_port == 0 || selected_port > n_ports) {
-        printf("\nNo location chosen, exiting.\n");
+    if (selected_port == -1) {
+        printf("No location chosen, exiting.\n");
         goto cleanup;
     }
 
-    char *port_name = sp_get_port_name(port_list[selected_port - 1]);
+    make_ft_write_command(command, selected_port);
+    command_status = system(command);
+    printf("\n");
 
-    char ft_command[1024];
-    make_ft_command(ft_command, serial);
+    if (command_status != 0) {
+        printf("There was an issue writing the new data to the device.\n");
+        printf("Please contact us at support@submodule.co.\n");
+        goto cleanup;
+    }
 
-    char avrdude_command[1024];
-    make_avrdude_command(avrdude_command, port_name);
-
-    printf("%s\n", ft_command);
-    system(ft_command);
-
-    /* printf("%s\n", avrdude_command); */
-
+    /* make_avrdude_command(command, port_name); */
+    /* printf("%s\n", command); */
 cleanup:
-    sp_free_port_list(port_list);
+    ;
 }
